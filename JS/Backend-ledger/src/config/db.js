e4a345edd18f3eefd5cbc2ff8d087dc1e2cef442
Dishant Dabi <dishantdabi04@gmail.com>
2026-06-20 21:16:29 +0530

    Initial commit

---
+++ JS/Backend-ledger/src/config/db.js
@@ -0,0 +1,20 @@
+//connect to DB
+const mongoose = require("mongoose")
+
+
+
+function connectToDB() {
+
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("server is connected to DB")
+        })
+        .catch(err => {
+            console.log("Error connecting to DB")
+            process.exit(1)
+        })
+
+}
+
+
+module.exports = connectToDB
